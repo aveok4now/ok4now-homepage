ecc4755a7ee37985b77ad135410cf45f2019350f
aveok4now <aveok4now@gmail.com>
2024-05-11 11:38:36 +0300

    feat: footer component

--- src/config.ts
+++ src/config.ts
@@ -2,3 +2,5 @@ export const AUTHOR_NAME = "@aveok4now";
 export const SITE_TITLE = "@aveok4now - Home Page";
 export const SITE_DESCRIPTION =
 	"A world of my projects, interests and achievements";
+export const GH_LINK = "https://github.com/aveok4now";
+export const SKIES_4NOW_LINK = "https://www.youtube.com/watch?v=iolngkQEp-Y&ab_channel=LilSkies"
