cf2ff9a9246f7137058905558513e1f8ac694e40
aveok4now <aveok4now@gmail.com>
2024-05-11 14:00:18 +0300

    refactor: move links to config

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,19 @@
+
+// Site Data
+
 export const AUTHOR_NAME = "@aveok4now";
 export const SITE_TITLE = "@aveok4now - Home Page";
 export const SITE_DESCRIPTION =
 	"A world of my projects, interests and achievements";
+
+// Social Links
 export const GH_LINK = "https://github.com/aveok4now";
+
+// Others
 export const SKIES_4NOW_LINK = "https://www.youtube.com/watch?v=iolngkQEp-Y&ab_channel=LilSkies"
+export const LARAVEL_LINK = "https://laravel.com";
+export const ASTRO_LINK = "https://docs.astro.build/";
+export const REACT_LINK = "https://react.dev/";
+export const VUE_LINK = "https://vuejs.org/";
+export const REACT_NATIVE_LINK = "https://reactnative.dev/";
+
