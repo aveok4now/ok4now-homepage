d2c13db20e16273d81a2be617d457071895aac2b
aveok4now <aveok4now@gmail.com>
2024-05-18 21:16:22 +0300

    style: implement DropdownMenu styles

--- src/components/DropdownMenu.tsx
+++ src/components/DropdownMenu.tsx
@@ -1,12 +1,12 @@
-import { Menu } from "lucide-react";
+import { HomeIcon, Menu, VenetianMask } from "lucide-react";
 import { useState } from "react";
 
 export function DropdownMenu() {
 	const [isOpen, setIsOpen] = useState(false);
 
 	const links = [
-		{ href: "/", label: "Home page" },
-		{ href: "/about", label: "About me" },
+		{ href: "/", label: "Home page", icon: HomeIcon },
+		{ href: "/about", label: "About me",  icon: VenetianMask },
 	];
 
 	return (
@@ -26,22 +26,24 @@ export function DropdownMenu() {
 
 			{isOpen && (
 				<div
-					className="absolute right-0 z-10 mt-9 w-32 text-start origin-top-right rounded-md bg-white/90 dark:bg-black/90 backdrop-blur-lg shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+					className="absolute right-0 z-10 mt-9 w-32 origin-top-right rounded-md bg-white/90 dark:bg-black/90 backdrop-blur-lg shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none transition-all duration-300"
 					role="menu"
 					aria-orientation="vertical"
 					aria-labelledby="menu-button"
 					tabIndex={-1}
 				>
-					<div role="none">
+					<div role="none" className="">
 						{links.map((link) => {
+							const Icon = link.icon;
 							return (
 								<a
 									key={link.href}
 									href={link.href}		
-									className="text-black dark:text-purple-300 block px-4 py-2 text-sm cursor-pointer hover:bg-purple-300/30 rounded-md"
+									className="text-black font-regular dark:text-purple-300 px-4 py-2 text-sm cursor-pointer hover:bg-purple-300/30 rounded-md flex items-center text-center gap-x-1.5"
 									role="menuitem"
 									tabIndex={-1}
 								>
+									<Icon size={12} className="text-black/50 dark:text-white"/>
 									{link.label}
 								</a>
 							);
